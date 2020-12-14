c8df96ee2d8758820df9c3892d0fb8ae878ec3b0
annaszyszkiewicz <250386@student.pwr.edu.pl>
2020-12-14 22:21:30 +0100

    Third commit

--- src/main/java/edu/ib/Calculate.java
+++ src/main/java/edu/ib/Calculate.java
@@ -1,67 +1,146 @@
 package edu.ib;
 
+/**
+ * klasa wykonujaca obliczenia
+ */
 public class Calculate {
 
+    private String value = "";
+
+    /**
+     * metoda zwracajaca wynik dodawania
+     * @param number wybrana liczba, ktora dodajemy
+     * @param previousNumber wynik poprzednich operacji, liczba do ktorej dodajemy
+     * @return wynik dodawania
+     */
     public String add(String number, String previousNumber){
         String result = String.valueOf(Double.parseDouble(previousNumber)+Double.parseDouble(number));
         if (result.endsWith(".0"))
             result = result.substring(0,result.length()-2);
+        value = result;
         return result;
     }
 
+    /**
+     * metoda zwracajaca wynik odejmowania
+     * @param number wybrana liczba, ktora odejmujemy
+     * @param previousNumber wynik poprzednich operacji, liczba od ktorej odejmujemy
+     * @return wynik odejmowania
+     */
     public String sub(String number, String previousNumber){
         String result = String.valueOf(Double.parseDouble(previousNumber)-Double.parseDouble(number));
         if (result.endsWith(".0"))
             result = result.substring(0,result.length()-2);
+        value = result;
         return result;
     }
 
+    /**
+     * metoda zwracajaca wynik dzielenia
+     * @param number wybrana liczba, przez ktora dzielimy
+     * @param previousNumber wynik poprzednich operacji, liczba ktora dzielimy
+     * @return wynik dzielenia
+     */
     public String div(String number, String previousNumber){
         String result = String.valueOf(Double.parseDouble(previousNumber)/Double.parseDouble(number));
         if (result.endsWith(".0"))
             result = result.substring(0,result.length()-2);
+        value = result;
         return result;
     }
 
+    /**
+     * metoda zwracajaca wynik mnozenia
+     * @param number wybrana liczba przez ktora mnozymy
+     * @param previousNumber wynik poprzednich operacji, liczba ktora mnozymy
+     * @return wynik mnozenia
+     */
     public String multi(String number, String previousNumber){
         String result = String.valueOf(Double.parseDouble(number)*Double.parseDouble(previousNumber));
         if (result.endsWith(".0"))
             result = result.substring(0,result.length()-2);
+        value = result;
         return result;
     }
 
+    /**
+     * metoda zwracajaca procent z liczby
+     * @param number wybrana liczba, ktora chcemy podzielic przez 100
+     * @return procent z liczby
+     */
     public String pc(String number){
         String result = String.valueOf(Double.parseDouble(number)/100);
         if (result.endsWith(".0"))
             result = result.substring(0,result.length()-2);
+        value = result;
         return result;
     }
 
+    /**
+     * metoda zwracajaco odwrotnosc liczby
+     * @param number wybrana liczba, ktorej chcemy odwrotnosc
+     * @return odwrotnosc liczby
+     */
     public String reverse(String number){
         String result = String.valueOf(1/Double.parseDouble(number));
         if (result.endsWith(".0"))
             result = result.substring(0,result.length()-2);
+        value = result;
         return result;
     }
 
+    /**
+     * metoda zwracajaca druga potege liczby
+     * @param number wybrana liczba, ktora chemy podnisc do kwadratu
+     * @return druga potega liczby
+     */
     public String pow(String number){
         String result = String.valueOf(Double.parseDouble(number)*Double.parseDouble(number));
         if (result.endsWith(".0"))
             result = result.substring(0,result.length()-2);
+        value = result;
         return result;
     }
 
+    /**
+     * metoda zwracajaca pierwiastek drugiego stopnia z liczby
+     * @param number wybrana liczba, ktora chcemy spierwiastkowac
+     * @return pierwiastek drugiego stopnia z liczby
+     */
     public String sqrt(String number){
         String result = String.valueOf(Math.sqrt(Double.parseDouble(number)));
         if (result.endsWith(".0"))
             result = result.substring(0,result.length()-2);
+        value = result;
         return result;
     }
 
+    /**
+     * metoda zwracajaca wybrana liczbe ze zmienionym znakiem
+     * @param number wybrana liczba, ktorej chcemy zmienic znak
+     * @return liczba ze zmienionym znakiem
+     */
     public String sign(String number){
         String result = String.valueOf(Double.parseDouble(number)-2*Double.parseDouble(number));
         if (result.endsWith(".0"))
             result = result.substring(0,result.length()-2);
+        value = result;
         return result;
+    }
+
+    /**
+     * metoda zwracajaca wynik po ostatniej operacji
+     * @return wartosc po ostatniej operacji
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * metoda ustawiajaca zmiane wyniku
+     * @param value wynik po ostatniej operacji
+     */
+    public void setValue(String value) {
+        this.value = value;
     }
 }
--- src/main/java/edu/ib/JavaFXCalculatorController.java
+++ src/main/java/edu/ib/JavaFXCalculatorController.java
@@ -6,15 +6,17 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+/**
+ * klasa zarzadzajaca wyswietlanym tekstem
+ */
 public class JavaFXCalculatorController {
+
     Calculate x = new Calculate();
 
-    private String operation = "+";
+    private String operation = "";
     private boolean previousStep = true;
-    private String result = "0";
 
     @FXML
     private ResourceBundle resources;
@@ -28,14 +30,55 @@ public class JavaFXCalculatorController {
     @FXML
     private TextField textNext;
 
-    @FXML
-    private TextArea textHistory;
 
+    /**
+     * metoda zmieniajaca widoczny tekst po kliknieciu "="
+     * dodaje wybrana ostatnio liczbe oraz  "=" do zapisanego rownania
+     * oraz powoduje wykonanie wybranego wcześniej działania
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void calculate(ActionEvent event) {
-
+        if (textNext.getText().contains("=")) {
+            String o = textNext.getText();
+            switch (operation) {
+                case "+" -> {
+                    textInput.setText(x.add(textInput.getText(), o.substring(o.lastIndexOf("+") + 1, o.length() - 1)));
+                    o = o.substring(o.lastIndexOf("+") + 1, o.length() - 1);
+                }
+                case "-" -> {
+                    textInput.setText(x.sub(textInput.getText(), o.substring(o.lastIndexOf("-") + 1, o.length() - 1)));
+                    o = o.substring(o.lastIndexOf("-") + 1, o.length() - 1);
+                }
+                case "/" -> {
+                    textInput.setText(x.div(textInput.getText(), o.substring(o.lastIndexOf("/") + 1, o.length() - 1)));
+                    o = o.substring(o.lastIndexOf("/") + 1, o.length() - 1);
+                }
+                case "*" -> {
+                    textInput.setText(x.multi(textInput.getText(), o.substring(o.lastIndexOf("*") + 1, o.length() - 1)));
+                    o = o.substring(o.lastIndexOf("*") + 1, o.length() - 1);
+                }
+            }
+            if (!textNext.getText().equals("0="))
+                textNext.setText(x.getValue() + operation + o + "=");
+        } else {
+            textNext.setText(textNext.getText() + textInput.getText() + "=");
+            switch (operation) {
+                case "+" -> textInput.setText(x.add(textInput.getText(), x.getValue()));
+                case "-" -> textInput.setText(x.sub(textInput.getText(), x.getValue()));
+                case "/" -> textInput.setText(x.div(textInput.getText(), x.getValue()));
+                case "*" -> textInput.setText(x.multi(textInput.getText(), x.getValue()));
+            }
+        }
     }
 
+    /**
+     * metoda zmieniajaca widoczny teskt po kliknięciu "<--"
+     * usuwa ostatnia widoczna cyfre jesli wynikiem operacji nie jest tylko 0
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void undo(ActionEvent event) {
         if (!textInput.getText().equals("0")) {
@@ -46,222 +89,332 @@ public class JavaFXCalculatorController {
         }
     }
 
+    /**
+     * metoda zmieniajaca widoczny teskt po kliknieciu "CE"
+     * usuwa ostatni wynik dzialan
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void clear(ActionEvent event) {
         textInput.setText("0");
-        if (operation == "=")
+        if (operation.equals("="))
             textNext.clear();
     }
 
+    /**
+     * metoda zmieniajaca widoczny teskt po kliknieciu "C"
+     * resetuje dotychczasowe operacje i ich wyniki
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void clearAll(ActionEvent event) {
         textInput.setText("0");
         textNext.clear();
-        result = "0";
+        x.setValue("");
+        operation = "";
     }
 
+    /**
+     * metoda zmieniajaca widoczny teskt po kliknieciu "+"
+     * dodaje wybrana ostatnio liczbe oraz  "+" do zapisanego rownania
+     * oraz wykonuje działanie dodawania
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void addition(ActionEvent event) {
-        if (previousStep && !textNext.getText().endsWith("+"))
-            textNext.setText(textNext.getText().substring(0, textNext.getLength() - 1) + "+");
-
-        else if (textNext.getText().endsWith(operation) && !previousStep) {
+        if (textNext.getText().contains("="))
+            textNext.setText(x.getValue() + "+");
+        else if (!previousStep || !operation.equals("+")) {
             textNext.setText(textNext.getText() + textInput.getText() + "+");
-            textInput.setText(result);
-
-        } else if (!previousStep)
-            textNext.setText(textNext.getText() + textInput.getText() + "+");
-
+            if (x.getValue().isEmpty())
+                x.setValue(textInput.getText());
+            else if (!textNext.getText().endsWith("+"))
+                textNext.setText(textNext.getText().substring(0, textNext.getLength() - 1) + "+");
+            else {
+                switch (operation) {
+                    case "+" -> textInput.setText(x.add(textInput.getText(), x.getValue()));
+                    case "-" -> textInput.setText(x.sub(textInput.getText(), x.getValue()));
+                    case "/" -> textInput.setText(x.div(textInput.getText(), x.getValue()));
+                    case "*" -> textInput.setText(x.multi(textInput.getText(), x.getValue()));
+                }
+            }
+        }
         operation = "+";
         previousStep = true;
-        result = textInput.getText();
     }
 
+    /**
+     * metoda zmieniajaca widoczny teskt po kliknieciu "-"
+     * dodaje wybrana ostatnio liczbe oraz  "-" do zapisanego rownania
+     * oraz wykonuje działanie odejmowania
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void subtraction(ActionEvent event) {
-        if (previousStep && !textNext.getText().endsWith("-"))
-            textNext.setText(textNext.getText().substring(0, textNext.getLength() - 1) + "-");
-
-        else if (textNext.getText().endsWith(operation) && !previousStep) {
+        if (textNext.getText().contains("="))
+            textNext.setText(x.getValue() + "-");
+        else if (!previousStep || !operation.equals("-")) {
             textNext.setText(textNext.getText() + textInput.getText() + "-");
-            textInput.setText(result);
+            if (x.getValue().isEmpty())
+                x.setValue(textInput.getText());
+            else if (!textNext.getText().endsWith("-"))
+                textNext.setText(textNext.getText().substring(0, textNext.getLength() - 1) + "-");
+            else {
+                switch (operation) {
+                    case "+" -> textInput.setText(x.add(textInput.getText(), x.getValue()));
+                    case "-" -> textInput.setText(x.sub(textInput.getText(), x.getValue()));
+                    case "/" -> textInput.setText(x.div(textInput.getText(), x.getValue()));
+                    case "*" -> textInput.setText(x.multi(textInput.getText(), x.getValue()));
+                }
+            }
 
-        } else if (!previousStep)
-            textNext.setText(textNext.getText() + textInput.getText() + "-");
-
+        }
         operation = "-";
         previousStep = true;
-        result = textInput.getText();
     }
 
+    /**
+     * metoda zmieniajaca widoczny teskt po kliknieciu "/"
+     * dodaje wybrana ostatnio liczbe oraz  "-" do zapisanego rownania
+     * oraz wykonuje działanie dzielenia
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void division(ActionEvent event) {
-        if (previousStep && !textNext.getText().endsWith("/"))
-            textNext.setText(textNext.getText().substring(0, textNext.getLength() - 1) + "/");
-
-        else if (textNext.getText().endsWith(operation) && !previousStep) {
+        if (textNext.getText().contains("="))
+            textNext.setText(x.getValue() + "/");
+        else if (!previousStep || !operation.equals("/")) {
             textNext.setText(textNext.getText() + textInput.getText() + "/");
-            //textInput.setText(result);
-
-        } else if (!previousStep)
-            textNext.setText(textNext.getText() + textInput.getText() + "/");
-
+            if (x.getValue().isEmpty())
+                x.setValue(textInput.getText());
+            else if (!textNext.getText().endsWith("/"))
+                textNext.setText(textNext.getText().substring(0, textNext.getLength() - 1) + "/");
+            else {
+                switch (operation) {
+                    case "+" -> textInput.setText(x.add(textInput.getText(), x.getValue()));
+                    case "-" -> textInput.setText(x.sub(textInput.getText(), x.getValue()));
+                    case "/" -> textInput.setText(x.div(textInput.getText(), x.getValue()));
+                    case "*" -> textInput.setText(x.multi(textInput.getText(), x.getValue()));
+                }
+            }
+        }
         operation = "/";
         previousStep = true;
-        //result = textInput.getText();
-        System.out.println(result);
     }
 
+    /**
+     * metoda zmieniajaca widoczny teskt po kliknieciu "*"
+     * dodaje wybrana ostatnio liczbe oraz  "*" do zapisanego rownania
+     * oraz wykonuje działanie mnożenia
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void multiplication(ActionEvent event) {
-        if (previousStep && !textNext.getText().endsWith("*"))
-            textNext.setText(textNext.getText().substring(0, textNext.getLength() - 1) + "*");
-
-       else if (textNext.getText().endsWith(operation) && !previousStep) {
+        if (textNext.getText().contains("="))
+            textNext.setText(x.getValue() + "*");
+        else if (!previousStep || !operation.equals("*")) {
             textNext.setText(textNext.getText() + textInput.getText() + "*");
-            textInput.setText(result);
-
-        } else if (!previousStep)
-            textNext.setText(textNext.getText() + textInput.getText() + "*");
-
+            if (x.getValue().isEmpty())
+                x.setValue(textInput.getText());
+            else if (!textNext.getText().endsWith("*"))
+                textNext.setText(textNext.getText().substring(0, textNext.getLength() - 1) + "*");
+            else {
+                switch (operation) {
+                    case "+" -> textInput.setText(x.add(textInput.getText(), x.getValue()));
+                    case "-" -> textInput.setText(x.sub(textInput.getText(), x.getValue()));
+                    case "/" -> textInput.setText(x.div(textInput.getText(), x.getValue()));
+                    case "*" -> textInput.setText(x.multi(textInput.getText(), x.getValue()));
+                }
+            }
+        }
         operation = "*";
         previousStep = true;
-        result = textInput.getText();
     }
 
+    /**
+     * metoda zmieniajaca widoczny teskt po kliknieciu "%"
+     * zamienia wybrana ostatnio liczbę na procent tej liczby (dzieli liczbę przez 100)
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void percent(ActionEvent event) {
         textInput.setText(x.pc(textInput.getText()));
         previousStep = true;
+        textNext.setText(textInput.getText());
     }
 
+    /**
+     * metoda zmieniajaca widoczny teskt po kliknieciu "1/x"
+     * zamienia wybrana ostatnio liczbę odwrotnosc tej liczby
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void reciprocal(ActionEvent event) {
         textInput.setText(x.reverse(textInput.getText()));
-        previousStep = true;
+        previousStep = false;
+
     }
 
+    /**
+     * metoda zmieniajaca widoczny teskt po kliknieciu "x^2"
+     * zamienia wybrana ostatnio liczbę na druga potege tej liczby
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void toSquare(ActionEvent event) {
         textInput.setText(x.pow(textInput.getText()));
-        previousStep = true;
+        previousStep = false;
     }
 
+    /**
+     * metoda zmieniajaca widoczny teskt po kliknieciu "sqrt(x)"
+     * zamienia wybrana ostatnio liczbę na pierwiastek drugiego stopnia tej liczby
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void extractRoot(ActionEvent event) {
         textInput.setText(x.sqrt(textInput.getText()));
-        previousStep = true;
+        previousStep = false;
+
     }
 
+    /**
+     * metoda zmieniajaca widoczny teskt po kliknieciu "+/-"
+     * zamienia znak wybranej licbzy na przeciwny
+     *
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void changeOfSign(ActionEvent event) {
         textInput.setText(x.sign(textInput.getText()));
         previousStep = false;
     }
 
+    /**
+     * dopisuje "0" do wpisywanej liczby lub zamienia wynik na "0"
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void zero(ActionEvent event) {
-        if (!textInput.getText().equals("0")) {
+        if (previousStep) {
+            textInput.setText("0");
+            previousStep = false;
+        } else if (textNext.getText().endsWith("=")) {
+            textInput.setText("0");
+            textNext.clear();
+            previousStep = false;
+        } else if (!textInput.getText().equals("0")) {
             textInput.setText(textInput.getText() + "0");
             previousStep = false;
         }
     }
 
+    /**
+     * dopisuje cyfrę do wpisywanej liczby lub zamienia wynik na wybraną cyfrę
+     * @param event przenosi informacje o wywołaniu metody
+     */
     @FXML
     void write(ActionEvent event) {
-        if (operation == "=") {
+        if (textNext.getText().endsWith("=")) {
+            textInput.clear();
             textNext.clear();
         }
+
         switch (((Button) event.getSource()).getText()) {
             case "1":
-                if (textInput.getText().equals("0") || previousStep == true) {
+                if (textInput.getText().equals("0") || previousStep)
                     textInput.setText("1");
-                } else {
+                else
                     textInput.setText(textInput.getText() + "1");
-                }
+
                 break;
             case "2":
-                if (textInput.getText().equals("0") || previousStep == true) {
+                if (textInput.getText().equals("0") || previousStep)
                     textInput.setText("2");
-                } else {
+                else
                     textInput.setText(textInput.getText() + "2");
-                }
+
                 break;
             case "3":
-                if (textInput.getText().equals("0") || previousStep) {
+                if (textInput.getText().equals("0") || previousStep)
                     textInput.setText("3");
-                } else {
+                else
                     textInput.setText(textInput.getText() + "3");
-                }
+
                 break;
             case "4":
-                if (textInput.getText().equals("0") || previousStep) {
+                if (textInput.getText().equals("0") || previousStep)
                     textInput.setText("4");
-                } else {
+                else
                     textInput.setText(textInput.getText() + "4");
-                }
+
                 break;
             case "5":
-                if (textInput.getText().equals("0") || previousStep) {
+                if (textInput.getText().equals("0") || previousStep)
                     textInput.setText("5");
-                } else {
+                else
                     textInput.setText(textInput.getText() + "5");
-                }
+
                 break;
             case "6":
-                if (textInput.getText().equals("0") || previousStep) {
+                if (textInput.getText().equals("0") || previousStep)
                     textInput.setText("6");
-                } else {
+                else
                     textInput.setText(textInput.getText() + "6");
-                }
+
                 break;
             case "7":
-                if (textInput.getText().equals("0") || previousStep == true) {
+                if (textInput.getText().equals("0") || previousStep)
                     textInput.setText("7");
-                } else {
+                else
                     textInput.setText(textInput.getText() + "7");
-                }
+
                 break;
             case "8":
-                if (textInput.getText().equals("0") || previousStep == true) {
+                if (textInput.getText().equals("0") || previousStep)
                     textInput.setText("8");
-                } else {
+                else
                     textInput.setText(textInput.getText() + "8");
-                }
+
                 break;
             case "9":
-                if (textInput.getText().equals("0") || previousStep) {
+                if (textInput.getText().equals("0") || previousStep)
                     textInput.setText("9");
-                } else {
+                else
                     textInput.setText(textInput.getText() + "9");
-                }
+
                 break;
             case ".":
-                if (!textInput.getText().contains("."))
+                if (!textInput.getText().contains(".")) {
                     if (textInput.getText().endsWith(operation) || previousStep)
                         textInput.setText("0.");
                     else
                         textInput.setText(textInput.getText() + ".");
-
+                }
                 break;
 
         }
+
         previousStep = false;
-        switch (operation) {
-            case "+" -> result = x.add(result, textInput.getText());
-            case "-" -> result = x.sub(result, textInput.getText());
-            case "/" -> result = x.div(result, textInput.getText());
-            case "*" -> result = x.multi(result, textInput.getText());
-        }
     }
 
-
+    /**
+     * metoda ustawiająca początkowe wartości programu
+     */
     @FXML
     void initialize() {
         assert textInput != null : "fx:id=\"textInput\" was not injected: check your FXML file 'calculator.fxml'.";
         assert textNext != null : "fx:id=\"textNext\" was not injected: check your FXML file 'calculator.fxml'.";
-        assert textHistory != null : "fx:id=\"textHistory\" was not injected: check your FXML file 'calculator.fxml'.";
-
     }
 }
 
